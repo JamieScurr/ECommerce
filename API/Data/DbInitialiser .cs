9fb024488e9628321c41458f5d335e4f0358bb52
Jamie Scurr <P2524989@my365.dmu.ac.uk>
2023-01-21 20:33:34 +0000

    Created Web API and Setup DB

---
+++ API/Data/DbInitialiser .cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Data
+{
+    public static class DbInitialiser //static does not require instance
+    {
+        public static void Initialise(StoreContext context)
+        {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Bing Chilling",
+                    Description =
+                        "A lightweight cream t-shirt perfect for chilling out.",
+                    Price = 25,
+                    PictureUrl = "/images/products/sb-ang1.png",
+                    Type = "Boards",
+                    QuantityInStock = 5
+                },
+
+                  new Product
+                {
+                    Name = "Best Knitting Mom Ever",
+                    Description =
+                        "Show some appreciation for you mom and her superb knitting skills!",
+                    Price = 25,
+                    PictureUrl = "/images/products/sb-ang1.png",
+                    Type = "Boards",
+                    QuantityInStock = 5
+                },
+
+            };
+
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+
+        }
+    }
+}
